e124f2620d83aea631ae3fc7e088712feb1ac3f5
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-11-22 17:30:48 +0100

    feature: Created a View for Registration

--- backend/career_guidance/users/views.py
+++ backend/career_guidance/users/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
 
 class TestView (APIView):
     def get(self,request):
         return Response({"message": "Hello, Django REST Framework!"})
+    
+class RegisterView(APIView):
+    def post(self, request):
+        serializer =RegisterSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save 
+            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
