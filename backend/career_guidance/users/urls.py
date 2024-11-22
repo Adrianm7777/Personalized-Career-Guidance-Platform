07920496aeae0d272ce2a1b6d82b5023ffca5f93
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-11-22 17:31:10 +0100

    feature: Added the Registration Route

--- backend/career_guidance/users/urls.py
+++ backend/career_guidance/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TestView
+from .views import TestView, RegisterView
+
 
 
 urlpatterns = [
-    path('test/',TestView.as_view(), name='test')
+    path('test/',TestView.as_view(), name='test'),
+    path('register', RegisterView.as_view(), name='register')
 ]
