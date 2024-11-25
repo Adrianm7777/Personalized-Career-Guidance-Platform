bec242d173dc68814b5ec82a77c39472c6ba18a3
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-11-25 17:14:55 +0100

    feature: Extended the User Model

--- backend/career_guidance/users/models.py
+++ backend/career_guidance/users/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    pass
+    bio = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
