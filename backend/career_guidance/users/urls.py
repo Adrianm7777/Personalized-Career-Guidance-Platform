85146c0424a1407c401a7acb5daf5f5cdee0485a
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-11-25 17:13:43 +0100

    feature: Added a Route for the Profile API

--- backend/career_guidance/users/urls.py
+++ backend/career_guidance/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import TestView, RegisterView
+from .views import RegisterView, UserProfileView
 
 
 
 urlpatterns = [
-    path('test/',TestView.as_view(), name='test'),
-    path('register', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', UserProfileView.as_view(), name='profile' ),
 ]
