af4a7d400a206637adf2704db061532a2b017db2
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-11-25 17:14:03 +0100

    feature: Created a View for the Profile API

--- backend/career_guidance/users/views.py
+++ backend/career_guidance/users/views.py
@@ -2,15 +2,26 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserProfileSerializer
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated
 
-class TestView (APIView):
-    def get(self,request):
-        return Response({"message": "Hello, Django REST Framework!"})
+User = get_user_model()
+
+class UserProfileView(RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserProfileSerializer
+
+    def get_object(self):
+        return self.request.user
     
 class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
-        serializer =RegisterSerializer(data = request.data)
+        serializer = RegisterSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save 
             return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
