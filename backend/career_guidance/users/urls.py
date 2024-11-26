76555b704c702ce65ad8b3ea86c11f6d56c66382
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-11-26 14:49:24 +0100

    feature: Added the Endpoint to urls.py

--- backend/career_guidance/users/urls.py
+++ backend/career_guidance/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RegisterView, UserProfileView
+from .views import RegisterView, UserProfileView, CareerGuidanceView
 
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', UserProfileView.as_view(), name='profile' ),
-]
+    path('career-guidance/', CareerGuidanceView.as_view(), name='career_guidance'),]
