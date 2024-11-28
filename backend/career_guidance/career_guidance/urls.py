07f75b70e51bf1b1e5dc77b653a8239648f22c43
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-11-28 15:42:36 +0100

    feature: Added URLs for registration and login endpoints.

--- backend/career_guidance/career_guidance/urls.py
+++ backend/career_guidance/career_guidance/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from backend.career_guidance.users.views import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/users/', include('users.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(),name='token_refresh')
 ]
