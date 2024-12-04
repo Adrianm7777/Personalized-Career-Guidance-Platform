453f7a3af864f0515e5f871df7d85145148e3812
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-12-04 16:34:37 +0100

    feature: created success registration page

---
+++ frontend/career-guidance-frontend/app/registration/successPage.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Registration Successful",
+};
+
+const successPage = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl font-bold mb-4">Registration Successful</h1>
+      <p>Thank you for registering! You can now log in to your account.</p>
+    </div>
+  );
+};
+
+export default successPage;
