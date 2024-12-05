5d827c620e5c2b3eadb151e43bdc3d9f3ec3118d
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-12-05 16:58:53 +0100

    fixed: eslint error

--- frontend/career-guidance-frontend/app/login/page.tsx
+++ frontend/career-guidance-frontend/app/login/page.tsx
@@ -40,7 +40,7 @@ const Login = () => {
           </button>
         </form>
         <p className="mt-4 text-center text-gray-600">
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <a
             href="/register"
             className="text-green-600 font-semibold hover:underline"
