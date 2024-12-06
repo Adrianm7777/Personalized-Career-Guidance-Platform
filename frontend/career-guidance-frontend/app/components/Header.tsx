4571ccad534d77cfa525632db4ae33b2af4824d6
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-12-06 16:22:36 +0100

    feature: improved styling

--- frontend/career-guidance-frontend/app/components/Header.tsx
+++ frontend/career-guidance-frontend/app/components/Header.tsx
@@ -4,15 +4,20 @@ import HomeIcon from "../assets/HomeIcon";
 
 const Header = () => {
   return (
-    <div className="w-screen h-14 fixed top-0 bg-gray-200 shadow-md flex justify-between items-center">
-      <div className="flex justify-center ml-3">
+    <div className="w-screen h-16 fixed top-0 bg-gray-900 text-white shadow-lg flex justify-between items-center px-4 sm:px-6 md:px-8">
+      <div className="flex justify-center items-center">
         <Link href="/">
-          <HomeIcon />
+          <div className="p-2 hover:bg-gray-700 rounded-full transition ease-in-out duration-200">
+            <HomeIcon />
+          </div>
         </Link>
       </div>
-      <div className="flex justify-center items-center mr-3">
+
+      <div className="flex justify-center items-center">
         <Link href="/registration">
-          <UserIcon />
+          <div className="p-2 hover:bg-gray-700 rounded-full transition ease-in-out duration-200">
+            <UserIcon />
+          </div>
         </Link>
       </div>
     </div>
